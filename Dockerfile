d5173c1d00df9d2e4a65587646eec94e6bd2eb97
Pieter E Smit <diepes@github.com>
2022-10-23 13:35:41 +1300

    add openssh-server to container entrypoint-sshd.sh print sshd to stderr

--- Dockerfile
+++ Dockerfile
@@ -13,6 +13,7 @@ RUN apt-get update \
            telnet \
            procps \
            less \
+           openssh-server \
     && rm -rf /var/lib/apt/lists/* \
     && echo "# apt done."
 
@@ -20,4 +21,5 @@ COPY entrypoint* /
 COPY motd /etc/motd
 RUN echo '[ ! -z "$TERM" -a -r /etc/motd ] && cat /etc/issue && cat /etc/motd' >> /etc/bash.bashrc
 
-CMD [ "/usr/bin/bash" ]
+CMD [ "/usr/bin/env", "bash" ]
+#CMD [ "/bin/sleep", "3600" ]
--- entrypoint-sshd.sh
+++ entrypoint-sshd.sh
@@ -2,13 +2,14 @@
 ## Entrypoint-sshd.sh installs and runs sshd, used for asible pipeline testing (Fake VM)
 # Activate with docker run -e "root_password=Passw0rd" --entrypoint /entrypoint-sshd.sh
 echo "#$0 installing openssh-server and allowing root ssh ..."
-apt-get update && apt-get install -y openssh-server
+#apt-get update 
+apt-get install -y openssh-server
 mkdir /var/run/sshd
 echo "root:${root_password:-Passw0rd}" | chpasswd
-sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/^.*PermitRootLogin .*$/PermitRootLogin yes/' /etc/ssh/sshd_config
 
 # SSH login fix. Otherwise user is kicked off after login
 sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
 
 echo "# running /usr/sbin/sshd deamon ..."
-/usr/sbin/sshd -D
+/usr/sbin/sshd -D -e
